7f609a531fb15dfab19aa52616d2b1a1ff11e3c7
yashrajsule <yashrajsule49@gmail.com>
2025-05-22 11:47:17 +0530

    Add files via upload

---
+++ Assignment_14/program1.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0, iCnt = 1;
+    
+    for(i = 1; i <= iRow; i++)
+    {
+    for ( j = 1; j <= iCol; j++,iCnt++)
+    {   
+        printf("%d\t",iCnt);
+
+        if(iCnt >= 9)
+        {
+        iCnt = 0;
+        }
+    }
+    printf("\n");
+    
+}
+
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows: \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns: \n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+
+
+}
